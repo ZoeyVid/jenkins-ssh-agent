d2b1f573777a1d8c3f163e1b14ac577aa39d55f1
Zoey <zoey@z0ey.de>
2023-01-21 20:19:11 +0100

    Update setup-sshd.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -35,12 +35,12 @@ write_key() {
   # As user, group, uid, gid and JENKINS_AGENT_HOME can be overridden at build,
   # we need to find the values for JENKINS_AGENT_HOME
   # ID_GROUP contains the user:group of JENKINS_AGENT_HOME directory
-  ID_GROUP=$(stat -c '%U:%G' "${JENKINS_AGENT_HOME}")
+  ID_GROUP=$(stat -c '%U:%G' "/home/jenkins")
 
-  mkdir -p "${JENKINS_AGENT_HOME}/.ssh"
-  echo "$1" > "${JENKINS_AGENT_HOME}/.ssh/authorized_keys"
-  chown -Rf "${ID_GROUP}" "${JENKINS_AGENT_HOME}/.ssh"
-  chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
+  mkdir -p "/home/jenkins/.ssh"
+  echo "$1" > "/home/jenkins/.ssh/authorized_keys"
+  chown -Rf "${ID_GROUP}" "/home/jenkins/.ssh"
+  chmod 0700 -R "/home/jenkins/.ssh"
 }
 
 if [[ ${JENKINS_AGENT_SSH_PUBKEY} == ssh-* ]]; then
