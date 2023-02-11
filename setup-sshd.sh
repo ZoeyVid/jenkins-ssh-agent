d8a406ba6da0cf55ddde2a94581d4b9203367f52
Zoey <zoey@z0ey.de>
2023-02-11 13:11:36 +0100

    Update setup-sshd.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -37,10 +37,10 @@ write_key() {
   # ID_GROUP contains the user:group of JENKINS_AGENT_HOME directory
   ID_GROUP=$(stat -c '%U:%G' "$JENKINS_AGENT_HOME")
 
-  mkdir -p ""$JENKINS_AGENT_HOME"/.ssh"
-  echo "$1" > ""$JENKINS_AGENT_HOME"/.ssh/authorized_keys"
-  chown -Rf "$ID_GROUP" ""$JENKINS_AGENT_HOME"/.ssh"
-  chmod 0700 -R ""$JENKINS_AGENT_HOME"/.ssh"
+  mkdir -p "$JENKINS_AGENT_HOME/.ssh"
+  echo "$1" > "$JENKINS_AGENT_HOME/.ssh/authorized_keys"
+  chown -Rf "$ID_GROUP" "$JENKINS_AGENT_HOME/.ssh"
+  chmod 0700 -R "$JENKINS_AGENT_HOME/.ssh"
 }
 
 if [[ "$JENKINS_AGENT_SSH_PUBKEY" == ssh-* ]]; then
